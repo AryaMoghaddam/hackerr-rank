dd2cbd5fa4efe35a29e296a721b62c0fd70f7031
Arya Moghaddam <ajavadim@uwaterloo.ca>
2022-04-05 17:04:52 -0400

    Create Plus Minus.py

---
+++ Plus Minus.py
@@ -0,0 +1,22 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+
+    arr = map(int, raw_input().rstrip().split())
+
+    plusMinus(arr)
